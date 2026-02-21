681b89ffcab499e7e5d894a51f52c65dd4849825
milay63bsm68-ops <milay63bsm68@gmail.com>
2026-02-21 23:05:26 +0100

    Create group 7881AB7423

--- groups.js
+++ groups.js
@@ -1,1 +1,22 @@
-window.GROUPS_DATA = {}
+window.GROUPS_DATA = {
+  "7881AB7423": {
+    "name": "Harmayohtest",
+    "description": "Test mayyoh",
+    "ownerId": "6940101627",
+    "ownerName": "Harmayoh",
+    "avatar": "{\"type\":\"emoji\",\"emoji\":\"⚡\",\"color\":\"#5288c1\"}",
+    "isPrivate": false,
+    "isPremiumOnly": false,
+    "createdAt": 1771711526569,
+    "lastMessageAt": null,
+    "lastMessage": null,
+    "totalEarnings": 0,
+    "members": {
+      "6940101627": {
+        "name": "Harmayoh",
+        "joinedAt": 1771711526569
+      }
+    },
+    "messages": []
+  }
+}
