fb05700f1a2209657f374c3e8477e4e446b5d9a8
milay63bsm68-ops <milay63bsm68@gmail.com>
2026-03-13 18:34:51 +0100

    Message in 0DF9B6E04A

--- groups.js
+++ groups.js
@@ -1884,7 +1884,7 @@ window.GROUPS_DATA = {
     "isPrivate": false,
     "isPremiumOnly": false,
     "createdAt": 1773423207133,
-    "lastMessageAt": 1773423268466,
+    "lastMessageAt": 1773423290789,
     "lastMessage": "🎤 𝙈𝙧 𝙎𝙤𝙡𝙪𝙩𝙞𝙤𝙣 𝐗❾: Voice note",
     "totalEarnings": 0,
     "members": {
@@ -1901,6 +1901,15 @@ window.GROUPS_DATA = {
         "senderName": "𝙈𝙧 𝙎𝙤𝙡𝙪𝙩𝙞𝙤𝙣 𝐗❾",
         "text": "How Una dey",
         "timestamp": 1773423257285
+      },
+      {
+        "id": "fc4639201a437241",
+        "type": "voice",
+        "senderId": "7100459393",
+        "senderName": "𝙈𝙧 𝙎𝙤𝙡𝙪𝙩𝙞𝙤𝙣 𝐗❾",
+        "duration": "0:06",
+        "audioUrl": "/api/audio/fc4639201a437241",
+        "timestamp": 1773423290789
       }
     ]
   }
